840f072df07cedf735ec1ca8bbd872be1d5c44a1
musthafa <musthafa.cp@kalvium.community>
2024-01-08 17:45:54 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Link, Route } from "react-router-dom";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+import Home from "./Component/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link className="kalvium" to="/home">KALVIUM❤️</Link>
+          </li>
+          <div>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
